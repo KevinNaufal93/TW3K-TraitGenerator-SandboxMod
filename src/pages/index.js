128181bb00bf438a012f681a5b32d1fd1d3b02a3
KevSprout <78581962+KevinNaufal93@users.noreply.github.com>
2023-02-27 12:43:14 +0700

    init next

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TW3K Trait's Generator</title>
+        <meta name="description" content="Powered by NextJs" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          Test
+        </div>
+      </main>
+    </>
+  )
+}
